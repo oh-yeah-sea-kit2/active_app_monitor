d615ce1e3f58284c14ff039143788d5860c7ca78
oh-yeah-sea-kit2 <kyohei.t.204@gmail.com>
2024-12-06 21:55:26 +0900

    アクティブなアプリを表示するようにしてみた

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,7 +3,23 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
+    override func applicationDidFinishLaunching(_ notification: Notification) {
+        let controller = self.mainFlutterWindow?.contentViewController as! FlutterViewController
+        let channel = FlutterMethodChannel(name: "com.example.active_app_display",
+                                           binaryMessenger: controller.engine.binaryMessenger)
+
+        channel.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) in
+            if call.method == "getActiveApp" {
+                if let activeApp = NSWorkspace.shared.frontmostApplication {
+                    result(activeApp.localizedName ?? "Unknown")
+                } else {
+                    result("No active application")
+                }
+            } else {
+                result(FlutterMethodNotImplemented)
+            }
+        }
+
+        super.applicationDidFinishLaunching(notification)
+    }
 }
